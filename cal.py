f87866dc093d69151e48b7eeb5009ceb834a5f72
maurya <maurya.sf@gmail.com>
2024-04-06 16:55:09 +0530

    Trig function done

--- cal.py
+++ cal.py
@@ -8,3 +8,8 @@ print("Square root of 16:", math.sqrt(16))
 print("Exponential function e^2:", math.exp(2))
 print("Logarithm base 10 of 100:", math.log10(100))
 print("Logarithm base e of 100:", math.log(100))
+
+# Trigonometric functions
+print("Sine of 30 degrees:", math.sin(math.radians(30)))
+print("Cosine of 45 degrees:", math.cos(math.radians(45)))
+print("Tangent of 60 degrees:", math.tan(math.radians(60)))
