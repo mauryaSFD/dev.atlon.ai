79e88d1fd1874b9f518368cb16dbc447cd1858c3
maurya <maurya.sf@gmail.com>
2024-04-06 16:47:33 +0530

    arth function done

--- cal.py
+++ cal.py
@@ -4,3 +4,7 @@ import math
 
 print("Pi:", math.pi)
 print("Euler's constant:", math.e)
+print("Square root of 16:", math.sqrt(16))
+print("Exponential function e^2:", math.exp(2))
+print("Logarithm base 10 of 100:", math.log10(100))
+print("Logarithm base e of 100:", math.log(100))
